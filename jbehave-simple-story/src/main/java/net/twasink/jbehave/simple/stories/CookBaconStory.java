19e21160fa71bc7aff8a6d5fd4deecfd16d9f8a6
Robert Watkins <robertdw@twasink.net>
2012-06-25 22:06:44 +1000

    Simplest possible story now working

--- jbehave-simple-story/src/main/java/net/twasink/jbehave/simple/stories/CookBaconStory.java
+++ jbehave-simple-story/src/main/java/net/twasink/jbehave/simple/stories/CookBaconStory.java
@@ -2,14 +2,19 @@ package net.twasink.jbehave.simple.stories;
 
 import net.twasink.jbehave.simple.steps.BaconCookingSteps;
 
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.junit.JUnitStory;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
 
 public class CookBaconStory extends JUnitStory {
 
-    @Override
-    public InjectableStepsFactory stepsFactory() {
+    @Override public Configuration configuration() {
+        return new MostUsefulConfiguration();
+    }
+    
+    @Override public InjectableStepsFactory stepsFactory() {
         return new InstanceStepsFactory(configuration(),
                 new BaconCookingSteps());
     }
