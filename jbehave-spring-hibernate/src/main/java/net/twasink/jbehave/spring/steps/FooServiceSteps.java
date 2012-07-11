4ec89399ea5ca125761f434b5a5d653d4083f0d1
Robert Watkins <robertdw@twasink.net>
2012-07-11 23:13:48 +1000

    Now spring-enabled

--- jbehave-spring-hibernate/src/main/java/net/twasink/jbehave/spring/steps/FooServiceSteps.java
+++ jbehave-spring-hibernate/src/main/java/net/twasink/jbehave/spring/steps/FooServiceSteps.java
@@ -1,18 +1,29 @@
 package net.twasink.jbehave.spring.steps;
 
+import static org.junit.Assert.assertEquals;
+import net.twasink.jbehave.spring.service.FooService;
+
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FooServiceSteps {
+    @Autowired
+    private FooService test;
+    @Autowired
+    private JdbcTemplate jdbc;
 
 
     @When("we create a Foo called 'bar'")
     public void createFoo() {
-        
+        test.createFoo("foo", "this is a test foo");     
     }
     
-    @Then("Then it gets saved in the database with the name 'bar'")
+    @Then("it gets saved in the database with the name 'bar'")
     public void lookForFoo() {
-        
+        assertEquals("we end with one foo", 1, jdbc.queryForInt("select count(*) from foo"));
     }
 }
--- jbehave-spring-hibernate/src/main/java/net/twasink/jbehave/spring/stories/FooServiceStory.java
+++ jbehave-spring-hibernate/src/main/java/net/twasink/jbehave/spring/stories/FooServiceStory.java
@@ -7,16 +7,33 @@ import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.junit.JUnitStory;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/testContext.xml")
 public class FooServiceStory extends JUnitStory {
+    @Autowired
+    private FooServiceSteps steps;
+    
+    @Autowired
+    private JdbcTemplate jdbc;
+    
+    @After
+    public void cleanData() {
+        jdbc.execute("truncate schema PUBLIC and commit");
+    }
 
     @Override public Configuration configuration() {
         return new MostUsefulConfiguration();
     }
     
     @Override public InjectableStepsFactory stepsFactory() {
-        return new InstanceStepsFactory(configuration(),
-                new FooServiceSteps());
+        return new InstanceStepsFactory(configuration(), steps);
     }
 
 }
